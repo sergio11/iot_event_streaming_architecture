e07006238975bc737b505c960b92a277d4a5fa46
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2019-06-07 22:49:22 +0200

    Initial Commit

---
+++ src/main/groovy/sanchez/sanchez/sergio/teveo/TeveoPlatformApplication.groovy
@@ -0,0 +1,14 @@
+package sanchez.sanchez.sergio.teveo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TeveoPlatformApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TeveoPlatformApplication, args)
+	
+	}
+
+}
