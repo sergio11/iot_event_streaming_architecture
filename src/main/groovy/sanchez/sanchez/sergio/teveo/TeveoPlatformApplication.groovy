092c93d1328458e95bc9f9e6ae582d0c19e81652
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2019-06-16 15:17:15 +0200

    "Configure cadvisor, nodeexporter and Spring actuator"

--- src/main/groovy/sanchez/sanchez/sergio/teveo/TeveoPlatformApplication.groovy
+++ src/main/groovy/sanchez/sanchez/sergio/teveo/TeveoPlatformApplication.groovy
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 
+
 @SpringBootApplication
 @EnableMongoAuditing
 class TeveoPlatformApplication {
