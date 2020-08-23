261d130f014dd303c3c0057c5c3e390ff7034f86
Sergio Sánchez <sss4esob@gmail.com>
2020-08-23 21:13:00 +0200

    Configure IOT Frames

--- kafka_connect/data/scripts/create_mongo_sink.sh
+++ kafka_connect/data/scripts/create_mongo_sink.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+curl -X POST -H "Content-Type: application/vnd.api+json" -H "Accept: application/vnd.api+json" \
+          --data-binary "@/data/scripts/config/iot_frames_topic.json"  \
+          "http://kafka-rest-proxy:8082/v3/clusters/F5NNZsLKQGWBGCj1oVu7hg/topics"
+
 # ---- Sink to MongoDB
 # Execute CURL command to create MongoDB Sink
 #
